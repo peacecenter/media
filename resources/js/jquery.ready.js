9416176d222c8d911c0f8a1ac680811b03d1f9a4
Jeremy <jdwilliams1991@gmail.com>
2018-10-29 15:06:43 -0400

    staging test

---
+++ resources/js/jquery.ready.js
@@ -0,0 +1,54 @@
+$(document).ready(function() {
+	jQuery("#all").click(function(){
+		$(".jumpBox .linkbox, .mediabox").removeClass("hidden");
+	});
+	$("#broadway").click(function(){
+		$(".jumpBox .linkbox, .mediabox").addClass("hidden");
+		$(".broadway").removeClass("hidden");
+	});
+	$("#singles").click(function(){
+		$(".jumpBox .linkbox, .mediabox").addClass("hidden");
+		$(".singles").removeClass("hidden");
+	});
+	$("#cultural").click(function(){
+		$(".jumpBox .linkbox, .mediabox").addClass("hidden");
+		$(".cultural").removeClass("hidden");
+	});
+	$("#community").click(function(){
+		$(".jumpBox .linkbox, .mediabox").addClass("hidden");
+		$(".community").removeClass("hidden");
+	});
+	$("#passport").click(function(){
+		$(".jumpBox .linkbox, .mediabox").addClass("hidden");
+		$(".passport").removeClass("hidden");
+	});
+	$("header .toggle").click(function(){
+		$(".mainMenu").toggleClass("active");
+		$(".subNav").slideUp();
+	});
+	$(".filter").click(function(){
+		$(".onFilter").removeClass("onFilter");
+		$(this).toggleClass("onFilter");
+	});
+	$(".hasSub").click(function(){
+		$(".subNav").slideToggle();
+	});
+	$(".toggle").click(function(){
+		if ($(".mainMenu").hasClass("active")) {
+			$(".toggle").text("Close Menu");
+		} else {
+			$(".toggle").text("Open Menu");
+		}
+	});
+	$(".return, .linkbox").on('click', function(event) {
+    if (this.hash !== "") {
+      event.preventDefault();
+      var hash = this.hash;
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+        window.location.hash = hash;
+      });
+    }
+  });
+});
